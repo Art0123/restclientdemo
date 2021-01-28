a43cc38a0a2c04f550c64fcda971297e07a31039
art01234 <magicdworf123@gmail.com>
2021-01-28 13:47:56 +0100

    initial commit

---
+++ src/test/java/com/art0123/restclientdemo/RestclientdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art0123.restclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
